5c49180f63253ff98e47524e6ade8f4c58a5fc65
Monky216 <90779085+Monky216@users.noreply.github.com>
2022-04-21 12:47:13 -0400

    Code

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+    public int cardNumber;
+    public enum CardSuit
+    {
+        Clubs,
+        Spades,
+        Diamonds,
+        Hearts
+    }
+}
